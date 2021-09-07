2ae0a539d5978aa3708bc41b6274c4d2b2c0e214
Mateusz Ziomek <mateuszziomek2000@gmail.com>
2021-09-07 17:59:38 +0200

    Add prettier and eslint rules to next js

--- web/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from 'next'
-
-const Home: NextPage = () => {
-  return (
-    <h1>App works</h1>
-  );
-};
-
-export default Home;
---
+++ web/src/pages/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return <h1>App works</h1>;
+};
+
+export default Home;
